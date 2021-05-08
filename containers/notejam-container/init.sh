decee4395def1909b14e078527f7656a22f7afac
Marc Bayón <marc.bayonbenegas@avl.com>
2021-05-08 15:12:29 +0200

    Updated ansible scripts + Ip of the notejam app

--- containers/notejam-container/init.sh
+++ containers/notejam-container/init.sh
@@ -3,6 +3,7 @@
 echo "Creating Database if it does not exits"
 echo -e "SELECT 'CREATE DATABASE notejam' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'notejam')\gexec" | psql "host=$DB_URL user=$DB_USER password=$DB_PASSWORD dbname=postgres sslmode=require"
 echo "Done"
+python /notejam/manage.py dumpdata --indent=2 auth > initial_data.json
 python /notejam/manage.py syncdb
 python /notejam/manage.py migrate
 python /notejam/manage.py runserver 0.0.0.0:8000
